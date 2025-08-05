ca9807085d2e4a53a1e4906aba2281e545d7ce72
Giwook-Han <hkw00011@gmail.com>
2025-08-05 18:55:35 +0900

    feat: add client/server feature flags to reduce dependencies

--- crates/prover/src/lib.rs
+++ crates/prover/src/lib.rs
@@ -1,9 +1,14 @@
+#[cfg(feature = "client")]
 mod client;
 mod message;
 mod request;
 mod response;
+#[cfg(feature = "server")]
 mod server;
 
+#[cfg(feature = "client")]
 pub use client::{ProverClient, ProverClientError};
+#[cfg(feature = "server")]
 pub use server::ProverServer;
+
 pub use request::ProverData;
