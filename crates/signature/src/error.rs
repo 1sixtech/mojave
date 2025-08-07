2b0b087e0b93894603696e2dd20bd5c056de7eeb
hungp2812 <hung05.pham@gmail.com>
2025-08-07 22:51:29 +0700

    fix

--- crates/signature/src/error.rs
+++ crates/signature/src/error.rs
@@ -3,10 +3,13 @@ pub enum SignatureError {
     #[cfg(feature = "secp256k1")]
     #[error("{0}")]
     Ecdsa(#[from] crate::ecdsa::Error),
-    #[cfg(feature = "ed25519")]
-    #[error("{0}")]
-    Eddsa(#[from] crate::eddsa::Error),
     #[cfg(feature = "secp256k1")]
     #[error("secp256k1 signature verification failed")]
     Secp256k1(#[from] secp256k1::Error),
+    #[cfg(feature = "ed25519")]
+    #[error("{0}")]
+    Eddsa(#[from] crate::eddsa::Error),
+    #[cfg(feature = "ed25519")]
+    #[error("ed25519 signature verification failed")]
+    Ed25519(#[from] ed25519_dalek::SignatureError),
 }
