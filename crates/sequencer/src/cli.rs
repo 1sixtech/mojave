9bf1a08cfe1890ac3c1146ece30a08c484a9dc41
dhlee <zombiedelah@gmail.com>
2025-08-08 12:47:14 +0900

    Add sequencer options

--- crates/sequencer/src/cli.rs
+++ crates/sequencer/src/cli.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, Subcommand};
+use clap::{ArgGroup, Parser, Subcommand};
 use mojave_chain_utils::options::Options;
 use tracing::Level;
 
@@ -36,5 +36,44 @@ pub enum Command {
     Init {
         #[command(flatten)]
         options: Options,
+        #[command(flatten)]
+        sequencer_options: SequencerOpts,
     },
+}
+
+#[derive(Parser)]
+#[clap(group(ArgGroup::new("mojave::SequencerOptions")))]
+pub struct SequencerOpts {
+    #[arg(
+        long = "full_node.addresses",
+        help = "Allowed domain(s) and port(s) for the sequencer in the form 'domain:port', can be specified multiple times",
+        help_heading = "Full Node Options",
+        default_value = "0.0.0.0:8545",
+        value_delimiter = ','
+    )]
+    pub full_node_addresses: Vec<String>,
+    #[arg(
+        long = "block_time",
+        help = "Block creation interval in milliseconds",
+        default_value = "1000"
+    )]
+    pub block_time: u64,
+}
+
+impl Default for SequencerOpts {
+    fn default() -> Self {
+        Self {
+            full_node_addresses: vec!["0.0.0.0:8545".to_string()],
+            block_time: 1000,
+        }
+    }
+}
+
+impl std::fmt::Debug for SequencerOpts {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("SequencerOptions")
+            .field("full_node_addresses", &self.full_node_addresses)
+            .field("block_time", &self.block_time)
+            .finish()
+    }
 }
