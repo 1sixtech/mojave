093077fa47495fdddc7f87e21008e8a345cf1518
Drizzle210 <anhduc5a1tp@gmail.com>
2025-08-11 23:28:16 +0700

    feat: setup integration tests structure

---
+++ crates/tests/tests/integration.rs
@@ -0,0 +1,29 @@
+fn test_setup() {
+
+}
+
+#[tokio::test]
+async fn test_sequencer_to_full_node_broadcast_block() {
+
+}
+
+#[tokio::test]
+async fn test_full_node_to_sequencer_forward_transaction() {
+
+}
+
+#[tokio::test]
+async fn test_network_error_handling_when_servers_unavailable() {
+
+}
+
+#[tokio::test]
+async fn test_forward_transaction() {
+
+}
+
+#[tokio::test]
+async fn test_send_block() {
+    
+}
+
