bb201ed553e64abf9123e55a80dbdebd09d9763c
Drizzle210 <anhduc5a1tp@gmail.com>
2025-08-18 12:40:34 +0700

    fix: update integration tests

--- crates/tests/tests/integration.rs
+++ crates/tests/tests/integration.rs
@@ -13,6 +13,7 @@ mod tests {
     };
     use mojave_client::MojaveClient;
     use mojave_tests::{start_test_api_full_node, start_test_api_sequencer};
+    use reqwest::Url;
     use secp256k1::SecretKey;
     use serde_json::{json, Value};
     use std::{
@@ -21,7 +22,6 @@ mod tests {
         time::{SystemTime, UNIX_EPOCH},
     };
     use tokio::time::{sleep, Duration};
-
     #[ctor]
     fn test_setup() {
         unsafe {
@@ -114,8 +114,10 @@ mod tests {
 
         // create mojave client and test block broadcast
         let private_key = std::env::var("PRIVATE_KEY").unwrap();
-        let client = MojaveClient::new(std::slice::from_ref(&server_url), &private_key).unwrap();
-        let result = client.send_broadcast_block(&test_block).await;
+        let client = MojaveClient::new(&private_key).unwrap();
+        let result = client
+            .send_broadcast_block(&test_block, vec![Url::parse(&server_url).unwrap()])
+            .await;
 
         server_handle.abort();
 
@@ -192,8 +194,10 @@ mod tests {
 
         // create mojave client and test block broadcast
         let private_key = std::env::var("PRIVATE_KEY").unwrap();
-        let client = MojaveClient::new(std::slice::from_ref(&server_url), &private_key).unwrap();
-        let result = client.send_broadcast_block(&test_block).await;
+        let client = MojaveClient::new(&private_key).unwrap();
+        let result = client
+            .send_broadcast_block(&test_block, vec![Url::parse(&server_url).unwrap()])
+            .await;
 
         // assert the response
         assert!(result.is_err(), "Should fail when server is unavailable");
@@ -337,6 +341,11 @@ mod tests {
             },
         };
 
-        sequencer_client.send_broadcast_block(&block).await.unwrap();
+        let full_node_urls = vec![Url::parse(&format!("http://{full_node_http_addr}")).unwrap()];
+
+        sequencer_client
+            .send_broadcast_block(&block, full_node_urls)
+            .await
+            .unwrap();
     }
 }
