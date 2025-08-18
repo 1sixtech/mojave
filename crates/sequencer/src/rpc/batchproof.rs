92999b60b4a57aed776a2129eff47c4da714c224
hungp2812 <hung05.pham@gmail.com>
2025-08-18 09:25:43 +0700

    refactor: update read request params

--- crates/sequencer/src/rpc/batchproof.rs
+++ crates/sequencer/src/rpc/batchproof.rs
@@ -1,24 +1,43 @@
 use crate::rpc::{RpcApiContext};
-use ethrex_common::types::{Block, BlockBody, Transaction};
 use ethrex_l2_common::prover::BatchProof;
 use ethrex_rpc::{
     RpcErr,
-    types::{block::RpcBlock, block_identifier::BlockIdentifier},
     utils::RpcRequest,
 };
-use mojave_client::types::SignedBlock;
-use mojave_signature::Verifier;
 use serde_json::Value;
 
-pub struct SendBatchProofRequest {
-    batch_proof: BatchProof
-}
+pub struct SendBatchProofRequest(BatchProof);
 
 impl SendBatchProofRequest {
-    pub async fn call(request: &RpcRequest, context: RpcApiContext) -> Result<Value, RpcErr> {
-        // Placeholder for actual batch proof logic
-        // This would involve fetching the necessary blocks and constructing the proof
+    fn get_batchproof(rpc_req_params: &Option<Vec<Value>>) -> Result<Self, RpcErr> {
+        let params = rpc_req_params
+            .as_ref()
+            .ok_or(RpcErr::BadParams("No params provided".to_owned()))?;
 
-        Ok(Value::Null) // Return an appropriate response
+        if params.len() != 1 {
+            return Err(RpcErr::BadParams(format!(
+                "Expected one param and {} were provided",
+                params.len()
+            )));
+        }
+
+        // Deserialize JSON → BatchProof
+        let proof = serde_json::from_value::<BatchProof>(params[0].clone())
+            .map_err(|e| RpcErr::BadParams(format!("Invalid BatchProof: {}", e)))?;
+
+        Ok(Self(proof))
+    }
+
+    pub async fn call(request: &RpcRequest, context: RpcApiContext) -> Result<Value, RpcErr> {
+        let data = Self::get_batchproof(&request.params)?;
+
+        // TODO: Sequencer logic
+        // e.g., context.proof_store.store(data.0).await?;
+
+        // data.0.proof();
+        
+        // For now, return success
+        serde_json::to_value("Proof accepted")
+            .map_err(|e| RpcErr::Internal(e.to_string()))
     }
 }
