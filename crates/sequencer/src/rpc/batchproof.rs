bca5aa6ac8e03998430dbce79a43924e50f3951c
hungp2812 <hung05.pham@gmail.com>
2025-08-18 11:03:13 +0700

    feat: save batchproof in rollup store

--- crates/sequencer/src/rpc/batchproof.rs
+++ crates/sequencer/src/rpc/batchproof.rs
@@ -6,7 +6,10 @@ use ethrex_rpc::{
 };
 use serde_json::Value;
 
-pub struct SendBatchProofRequest(BatchProof);
+pub struct SendBatchProofRequest {
+    batch_number: u64,
+    proof: BatchProof,
+}
 
 impl SendBatchProofRequest {
     fn get_batchproof(rpc_req_params: &Option<Vec<Value>>) -> Result<Self, RpcErr> {
@@ -14,27 +17,35 @@ impl SendBatchProofRequest {
             .as_ref()
             .ok_or(RpcErr::BadParams("No params provided".to_owned()))?;
 
-        if params.len() != 1 {
+        if params.len() != 2 {
             return Err(RpcErr::BadParams(format!(
-                "Expected one param and {} were provided",
+                "Expected two params (batch_number, proof) but {} were provided",
                 params.len()
             )));
         }
 
-        // Deserialize JSON → BatchProof
-        let proof = serde_json::from_value::<BatchProof>(params[0].clone())
+        let batch_number = serde_json::from_value::<u64>(params[0].clone())
+            .map_err(|e| RpcErr::BadParams(format!("Invalid batch_number: {}", e)))?;
+
+        let proof = serde_json::from_value::<BatchProof>(params[1].clone())
             .map_err(|e| RpcErr::BadParams(format!("Invalid BatchProof: {}", e)))?;
 
-        Ok(Self(proof))
+        Ok(Self {
+            batch_number,
+            proof,
+        })
     }
 
     pub async fn call(request: &RpcRequest, context: RpcApiContext) -> Result<Value, RpcErr> {
         let data = Self::get_batchproof(&request.params)?;
 
-        // TODO: Sequencer logic
-        // e.g., context.proof_store.store(data.0).await?;
+        let proof_type = data.proof.prover_type();
 
-        // data.0.proof();
+        context
+            .rollup_store
+            .store_proof_by_batch_and_type(data.batch_number, proof_type, data.proof.clone())
+            .await
+            .map_err(|e| RpcErr::Internal(format!("Failed to store proof: {}", e)))?;
         
         // For now, return success
         serde_json::to_value("Proof accepted")
