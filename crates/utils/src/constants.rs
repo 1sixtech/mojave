8408b0b9ea63d7216d880e8860e64aba960ddd15
Sacha Froment <sfroment42@gmail.com>
2025-09-02 09:42:24 +0200

    feat: add multi in btc watcher (#118)
    * feat: add multi in btc watcher
    
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>
    
    * chore: forgot file
    
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>
    
    * chore: better error
    
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>
    
    ---------
    
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>

--- crates/utils/src/constants.rs
+++ crates/utils/src/constants.rs
@@ -2,3 +2,4 @@
 /// We expect at least 2 frames: [topic, payload].
 pub const ZMQ_MESSAGE_MIN_FRAMES: usize = 2;
 pub const ZMQ_PAYLOAD_FRAME_INDEX: usize = 1;
+pub const ZMQ_TOPIC_FRAME_INDEX: usize = 0;
