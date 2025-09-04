37c592d963369313c76c99e86e321d6e6a909e8c
Sacha Froment <sfroment42@gmail.com>
2025-09-04 15:19:55 +0200

    feat: add rpc-core
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>

---
+++ crates/rpc-core/src/utils.rs
@@ -0,0 +1,36 @@
+use crate::{
+    RpcErr, RpcErrorResponse, RpcRequest, RpcRequestId, RpcSuccessResponse, types::Namespace,
+};
+use serde_json::Value;
+
+pub fn rpc_response(id: RpcRequestId, res: Result<Value, RpcErr>) -> Result<Value, RpcErr> {
+    Ok(match res {
+        Ok(result) => serde_json::to_value(RpcSuccessResponse {
+            id,
+            jsonrpc: "2.0".to_string(),
+            result,
+        }),
+        Err(error) => serde_json::to_value(RpcErrorResponse {
+            id,
+            jsonrpc: "2.0".to_string(),
+            error: error.into(),
+        }),
+    }?)
+}
+
+pub fn resolve_namespace(req: &RpcRequest) -> Result<Namespace, RpcErr> {
+    let req_method = req.method.replace('\"', "");
+    let mut parts = req_method.split('_');
+    let Some(namespace) = parts.next() else {
+        return Err(RpcErr::MethodNotFound(req.method.clone()));
+    };
+    match namespace {
+        "debug" => Ok(Namespace::Debug),
+        "eth" => Ok(Namespace::Eth),
+        "moj" => Ok(Namespace::Mojave),
+        "net" => Ok(Namespace::Net),
+        "txpool" => Ok(Namespace::TxPool),
+        "web3" => Ok(Namespace::Web3),
+        _others => Err(RpcErr::MethodNotFound(req.method.clone())),
+    }
+}
