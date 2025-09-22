9e825da827da56559baa6a8f2beb31cdf9a55109
Sacha Froment <sfroment42@gmail.com>
2025-09-22 11:20:02 +0200

    chore: only one const
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>

--- crates/batch-submitter/src/builder.rs
+++ crates/batch-submitter/src/builder.rs
@@ -18,6 +18,8 @@ use rand::{RngCore, rngs::OsRng};
 
 use crate::error::{Error, Result};
 
+const MAX_PUSH_SIZE: usize = 520;
+
 pub struct BuilderContext {
     pub rpc_client: BitcoinRPCClient,
     pub fee_rate: FeeRate,
@@ -139,7 +141,6 @@ fn build_reveal_script(taproot_public_key: &XOnlyPublicKey, payload: &[u8]) -> R
         .push_opcode(bitcoin::opcodes::OP_FALSE)
         .push_opcode(bitcoin::opcodes::all::OP_IF);
 
-    const MAX_PUSH_SIZE: usize = 520;
     for chunk in payload.chunks(MAX_PUSH_SIZE) {
         script_builder = script_builder.push_slice(
             script::PushBytesBuf::try_from(chunk.to_vec())
@@ -317,7 +318,6 @@ mod tests {
             .push_opcode(opcodes::OP_FALSE)
             .push_opcode(opcodes::all::OP_IF);
 
-        const MAX_PUSH_SIZE: usize = 520;
         for chunk in long_payload.chunks(MAX_PUSH_SIZE) {
             expected_script_builder =
                 expected_script_builder.push_slice(PushBytesBuf::try_from(chunk.to_vec()).unwrap());
