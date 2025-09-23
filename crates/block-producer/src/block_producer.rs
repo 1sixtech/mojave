ec450e453d7691d85dafeb33f649eacd58cfea0c
Drizzle210 <anhduc5a1tp@gmail.com>
2025-09-23 15:48:35 +0700

    refactor: adapt mojave task to block producer

--- crates/block-producer/src/block_producer.rs
+++ crates/block-producer/src/block_producer.rs
@@ -1,17 +1,12 @@
 use crate::{
     BlockProducerContext,
-    error::{Error, Result},
+    error::{Result},
     types::BlockProducerOptions,
 };
-use ethrex_common::types::Block;
-use mojave_node_lib::types::MojaveNode;
 use std::time::Duration;
-use tokio::sync::{
-    mpsc::{self, error::TrySendError},
-    oneshot,
-};
-use tokio_stream::{StreamExt, wrappers::ReceiverStream};
-use tracing::error;
+use mojave_task::Task;
+use mojave_node_lib::types::MojaveNode;
+use ethrex_common::types::Block;
 
 pub async fn run(node: MojaveNode, block_producer_options: &BlockProducerOptions) -> Result<()> {
     let context = BlockProducerContext::new(
@@ -21,71 +16,59 @@ pub async fn run(node: MojaveNode, block_producer_options: &BlockProducerOptions
         node.genesis.coinbase,
     );
     let block_time = block_producer_options.block_time;
-    let block_producer = BlockProducer::start(context, 100);
+    let block_producer = BlockProducer::new(context).spawn_with_capacity(100);
 
-    let cancel_token = node.cancel_token.clone();
-
-    tokio::select! {
-        _ = async {
-            loop {
-                if let Err(error) = block_producer.build_block().await {
-                    tracing::error!("Failed to build a block: {}", error);
-                    break;
-                }
-                tokio::time::sleep(Duration::from_millis(block_time)).await;
-            }
-        } => {}
-        _ = cancel_token.cancelled() => {
-            tracing::info!("Shutting down block producer");
+    let block_producer_for_loop = block_producer.clone();
+    tokio::spawn(async move {
+        loop {
+            let response = block_producer_for_loop.request(Request::BuildBlock).await.unwrap();
+            tracing::info!("Block built: {response:?}");
+            tokio::time::sleep(Duration::from_millis(block_time)).await;
         }
-    }
+    });
 
+    mojave_utils::signal::wait_for_shutdown_signal().await.unwrap();
+    block_producer.shutdown().await.unwrap();
     Ok(())
 }
 
-#[derive(Clone)]
 pub struct BlockProducer {
-    sender: mpsc::Sender<Message>,
+    context: BlockProducerContext, // TODO: do we need this?
 }
 
 impl BlockProducer {
-    pub fn start(context: BlockProducerContext, channel_capacity: usize) -> Self {
-        let (sender, receiver) = mpsc::channel(channel_capacity);
-        let mut receiver = ReceiverStream::new(receiver);
-
-        tokio::spawn(async move {
-            while let Some(message) = receiver.next().await {
-                handle_message(&context, message).await;
-            }
-
-            error!("Block builder stopped because the sender dropped.");
-        });
-        Self { sender }
-    }
-
-    pub async fn build_block(&self) -> Result<Block> {
-        let (sender, receiver) = oneshot::channel();
-        self.sender
-            .try_send(Message::BuildBlock(sender))
-            .map_err(|error| match error {
-                TrySendError::Full(_) => Error::Full,
-                TrySendError::Closed(_) => Error::Stopped,
-            })?;
-        receiver.await?
-    }
-}
-
-async fn handle_message(context: &BlockProducerContext, message: Message) {
-    match message {
-        Message::BuildBlock(sender) => {
-            if let Err(e) = sender.send(context.build_block().await) {
-                tracing::warn!(error = ?e, "Failed to send built block over channel");
-            }
+    pub fn new(context: BlockProducerContext) -> Self {
+        Self {
+            context,
         }
     }
 }
 
-#[allow(clippy::large_enum_variant)]
-enum Message {
-    BuildBlock(oneshot::Sender<Result<Block>>),
+
+impl mojave_task::Task for BlockProducer {
+    type Request = Request;
+    type Response = Response;
+    type Error = crate::error::Error;
+
+    async fn handle_request(&self, request: Request) -> Result<Self::Response> {
+        match request {
+            Request::BuildBlock => Ok(Response::Block(self.context.build_block().await?)),
+        }
+    }
+
+    async fn on_shutdown(&self) -> Result<()> {
+        tracing::info!("Shutting down block producer");
+        Ok(())
+    }
+}
+
+
+
+pub enum Request {
+    BuildBlock,
+}
+
+#[derive(Debug)]
+pub enum Response {
+    Block(Block),
 }
