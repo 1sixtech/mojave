221962733d7e389e522ce260a7057681ff8b3df2
Drizzle210 <anhduc5a1tp@gmail.com>
2025-09-25 11:11:54 +0700

    feat: refactor block producer for more clearly

--- cmd/sequencer/src/main.rs
+++ cmd/sequencer/src/main.rs
@@ -3,9 +3,12 @@ pub mod cli;
 use crate::cli::Command;
 use anyhow::Result;
 
-use mojave_batch_producer::{BatchProducer, types::Request};
+use mojave_batch_producer::{BatchProducer, types::Request as BatchProducerRequest};
 use mojave_batch_submitter::{committer::Committer, notifier::Notifier};
-use mojave_block_producer::types::BlockProducerOptions;
+use mojave_block_producer::{
+    BlockProducer,
+    types::{BlockProducerOptions, Request as BlockProducerRequest},
+};
 use mojave_node_lib::{initializers::get_signer, types::MojaveNode};
 use mojave_proof_coordinator::types::ProofCoordinatorOptions;
 use mojave_task::Task;
@@ -18,6 +21,7 @@ use std::{path::PathBuf, time::Duration};
 
 const PID_FILE_NAME: &str = "sequencer.pid";
 const LOG_FILE_NAME: &str = "sequencer.log";
+const BLOCK_PRODUCER_CAPACITY: usize = 100;
 
 fn main() -> Result<()> {
     mojave_utils::logging::init();
@@ -45,15 +49,10 @@ fn main() -> Result<()> {
                 let node = MojaveNode::init(&node_options)
                     .await
                     .map_err(|e| Box::new(e) as Box<dyn std::error::Error>)?;
-
-                let batch_producer = BatchProducer::new(node.clone(), 0);
-
                 let cancel_token = node.cancel_token.clone();
 
-                let mut block_producer_task = Box::pin(mojave_block_producer::run(
-                    node.clone(),
-                    &block_producer_options,
-                ));
+                let batch_producer = BatchProducer::new(node.clone(), 0);
+                let block_producer = BlockProducer::new(node.clone());
 
                 let (batch_tx, batch_rx) = tokio::sync::mpsc::channel(16);
 
@@ -65,22 +64,21 @@ fn main() -> Result<()> {
                 ));
 
                 let batch_producer_task = batch_producer
-                    .spawn_periodic(Duration::from_millis(10_000), || Request::BuildBatch);
+                    .spawn_periodic(Duration::from_millis(10_000), || BatchProducerRequest::BuildBatch);
+
+                let block_producer_task = block_producer
+                    .spawn_with_capacity_periodic(BLOCK_PRODUCER_CAPACITY, Duration::from_millis(block_producer_options.block_time), || BlockProducerRequest::BuildBlock);
 
                 let batch_submitter_task = Box::pin(Committer::<Notifier>::run(batch_tx));
 
                 tokio::select! {
-                    res = &mut block_producer_task => {
-                        res.map_err(|e| Box::new(e) as Box<dyn std::error::Error>)
-                    }
+                    // TODO: replace with api task
 
                     _ = mojave_utils::signal::wait_for_shutdown_signal()  => {
                         tracing::info!("Termination signal received, shutting down sequencer..");
                         cancel_token.cancel();
                         batch_producer_task.shutdown().await.map_err(|e| Box::new(e) as Box<dyn std::error::Error>)?;
-                        block_producer_task
-                            .await
-                            .map_err(|e| Box::new(e) as Box<dyn std::error::Error>)?;
+                        block_producer_task.shutdown().await.map_err(|e| Box::new(e) as Box<dyn std::error::Error>)?;
                         coordinator_task
                             .await
                             .map_err(|e| Box::new(e) as Box<dyn std::error::Error>)?;
--- crates/block-producer/src/lib.rs
+++ crates/block-producer/src/lib.rs
@@ -1,11 +1,9 @@
 mod block_producer;
 mod error;
-mod service;
 
 pub mod types;
 
 pub use block_producer::BlockProducer;
-pub use service::run;
 
 pub mod prelude {
     pub use crate::{
