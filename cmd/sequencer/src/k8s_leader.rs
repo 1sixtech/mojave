4c21205528f1e5bb1711507d9de4a1c87f630bfc
Giwook-Han <hkw00011@gmail.com>
2025-11-11 15:45:43 +0900

    mod: add k8s leader shutdown

--- cmd/sequencer/src/k8s_leader.rs
+++ cmd/sequencer/src/k8s_leader.rs
@@ -1,6 +1,6 @@
 use kube::Client;
 use kube_leader_election::{LeaseLock, LeaseLockParams};
-use std::{env, time::Duration};
+use std::{env, path::PathBuf, result::Result::*, time::Duration};
 use tokio::time::sleep;
 
 use mojave_batch_producer::{BatchProducer, types::Request as BatchProducerRequest};
@@ -9,7 +9,10 @@ use mojave_block_producer::{
     BlockProducer,
     types::{BlockProducerOptions, Request as BlockProducerRequest},
 };
-use mojave_node_lib::types::MojaveNode;
+use mojave_node_lib::{
+    types::{MojaveNode, NodeConfigFile},
+    utils::store_node_config_file,
+};
 use mojave_proof_coordinator::{ProofCoordinator, types::ProofCoordinatorOptions};
 use mojave_task::{Runner, Task, TaskHandle};
 use tracing::{error, info};
@@ -51,36 +54,60 @@ pub async fn run_with_k8s_coordination(
     let mut leader_tasks: Option<LeaderTasks> = None;
 
     loop {
-        match lease_lock.try_acquire_or_renew().await {
-            Ok(res) => {
-                let became_leader = res.acquired_lease;
-
-                if !am_i_leader && became_leader {
-                    // GW - Do I need to add Sleep here to wait all the leader task stop?
-
-                    info!("Became a leader. Start leader tasks");
-                    leader_tasks = Some(
-                        start_leader_tasks(
-                            node.clone(),
-                            &node_options,
-                            &block_producer_options,
-                            &proof_coordinator_options,
-                        )
-                        .await?,
-                    );
-                    am_i_leader = true;
-                } else if !became_leader && am_i_leader {
-                    info!("Became a follower. Stop leader tasks");
+        tokio::select! {
+            _ = mojave_utils::signal::wait_for_shutdown_signal() => {
+                info!("Termination signal received (K8s). Stopping leader tasks and exiting...");
+                if am_i_leader {
                     if let Some(lt) = leader_tasks.take() {
                         stop_leader_tasks(lt).await?;
                     }
-                    am_i_leader = false;
+                    if let Err(err) = lease_lock.step_down().await {
+                        error!("Error while stepping down from leader: {err:?}");
+                    }
                 }
 
-                sleep(Duration::from_secs(renew_every_secs)).await;
+                let node_config_path = PathBuf::from(node.data_dir.clone()).join("node_config.json");
+                info!("Storing config at {:?}...", node_config_path);
+                let node_config = NodeConfigFile::new(node.peer_table.clone(), node.local_node_record.lock().await.clone()).await;
+                store_node_config_file(node_config, node_config_path).await;
+
+                info!("Shutdown complete.");
+                break Ok(());
             }
-            Err(err) => {
-                error!("Error while k8s leader election: {err:}")
+            res = lease_lock.try_acquire_or_renew() => {
+                match res {
+                    Ok(res) => {
+                        let became_leader = res.acquired_lease;
+
+                        if !am_i_leader && became_leader {
+                            // GW - Do I need to add Sleep here to wait all the leader task stop?
+
+                            info!("Became a leader. Start leader tasks");
+                            leader_tasks = Some(
+                                start_leader_tasks(
+                                    node.clone(),
+                                    &node_options,
+                                    &block_producer_options,
+                                    &proof_coordinator_options,
+                                )
+                                .await?,
+                            );
+                            am_i_leader = true;
+                        } else if !became_leader && am_i_leader {
+                            info!("Became a follower. Stop leader tasks");
+                            if let Some(lt) = leader_tasks.take() {
+                                stop_leader_tasks(lt).await?;
+                            }
+                            am_i_leader = false;
+                        }
+
+                        sleep(Duration::from_secs(renew_every_secs)).await;
+                    }
+                    Err(err) => {
+                        error!("Error while k8s leader election: {err:?}");
+                        break Err(Box::new(err));
+                    }
+                }
             }
         }
     }
